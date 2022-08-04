56df855e426824300281a7a7f465416f34dc5462
SethPeacock <msethpeacock@gmail.com>
2022-08-04 14:03:08 -0700

    eda-seth to eda-first-set

--- analysis/eda/eda-first-set.py
+++ analysis/eda/eda-first-set.py
@@ -4,14 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from Pil import Image
-import statsmodels.api as sm
 import os
-import h5py
 
 data_path = ['..', '..', 'data']
 
-# %%
 # # Synth Data First Set
+# %% 
 
 data_1 = pd.read_table(os.path.join(*data_path, 'SyntheticData_FirstSet.txt'),
                        delimiter='   ',
@@ -25,18 +23,18 @@ motif_labels = pd.read_table(os.path.join(*data_path, 'membership.txt'),
                              engine='python')
 
 data_1.insert(0, 'motif', motif_labels[0])
-# %%
-
-# Empty first-set-heatmaps folder
+# %% Delete all files in the first-set-heatmaps folder
 fs_hm_path = 'first-set-heatmaps'
 file_list = [ f for f in os.listdir(fs_hm_path)]
 for f in file_list:
     os.remove(os.path.join(fs_hm_path, f))
 
-# %%
+# %% Sample of heatmaps
+
 i_2nd = {}
 for i in data_1['motif'].unique():
     i_2nd[i] = 0
+
 for index in data_1.groupby('motif').sample(n=10, random_state=1).index:
     temp_matrix = data_1.loc[index, 0:].to_numpy().reshape(51,51)
     hm = sns.heatmap(
@@ -52,14 +50,10 @@ for index in data_1.groupby('motif').sample(n=10, random_state=1).index:
     plt.clf()
     i_2nd[motif] += 1
 
-# %%
+# %% Convert images into a pdf
 images = []
 file_list = [ f for f in os.listdir(fs_hm_path)]
 for f in file_list:
     images.append(Image.open(os.path.join(fs_hm_path, f)).convert('RGB'))
 
 images[0].save(os.path.join(fs_hm_path, 'first-set-heatmaps.pdf'), save_all=True, append_images=images[1:])
-
-# %%
-gaus_conv_matrix
-
