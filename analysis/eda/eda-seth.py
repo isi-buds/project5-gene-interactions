4e409f1972b58a7e7a3b57d4c77ae279b89ad99e
SethPeacock <msethpeacock@gmail.com>
2022-08-04 13:54:56 -0700

    updated heatmaps

--- analysis/eda/eda-seth.py
+++ analysis/eda/eda-seth.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from Pil import Image
+import statsmodels.api as sm
 import os
 import h5py
 
@@ -12,31 +13,44 @@ data_path = ['..', '..', 'data']
 # %%
 # # Synth Data First Set
 
-data_1 = pd.read_table(os.path.join(*data_path, 'SyntheticData_FirstSet.txt'), delimiter='   ', header=None, engine='python')
+data_1 = pd.read_table(os.path.join(*data_path, 'SyntheticData_FirstSet.txt'),
+                       delimiter='   ',
+                       header=None,
+                       engine='python')
 
+motif_labels = pd.read_table(os.path.join(*data_path, 'membership.txt'),
+                             delimiter='   ',
+                             header=None,
+                             dtype=int,
+                             engine='python')
+
+data_1.insert(0, 'motif', motif_labels[0])
 # %%
+
 # Empty first-set-heatmaps folder
 fs_hm_path = 'first-set-heatmaps'
 file_list = [ f for f in os.listdir(fs_hm_path)]
 for f in file_list:
     os.remove(os.path.join(fs_hm_path, f))
+
 # %%
-secondary_i = 0
-for index in range(data_1.shape[0]):
-    temp_matrix = data_1.iloc[index, :].to_numpy().reshape(51,51)
-    condition = temp_matrix[0:3, 0:3].sum() < .01
-    if condition:
-        hm = sns.heatmap(
-            temp_matrix,
-        )
-        hm.invert_yaxis()
-        file_name = '{sec_i:0{width_2}}-index-{index:0{width_1}}'.format(index=index,
-                                                                width_1=4, 
-                                                                sec_i=secondary_i, 
-                                                                width_2=4)
-        plt.savefig(os.path.join(fs_hm_path, file_name))
-        plt.clf()
-        secondary_i += 1
+i_2nd = {}
+for i in data_1['motif'].unique():
+    i_2nd[i] = 0
+for index in data_1.groupby('motif').sample(n=10, random_state=1).index:
+    temp_matrix = data_1.loc[index, 0:].to_numpy().reshape(51,51)
+    hm = sns.heatmap(
+        temp_matrix,
+    )
+    hm.invert_yaxis()
+    motif = data_1.at[index, 'motif']
+    file_name = '{motif:02}-{i_2nd:02}-index{index}'.format(motif=motif,
+                                                            i_2nd=i_2nd[motif],
+                                                            index=index)
+    plt.title(file_name)
+    plt.savefig(os.path.join(fs_hm_path, file_name))
+    plt.clf()
+    i_2nd[motif] += 1
 
 # %%
 images = []
@@ -47,4 +61,5 @@ for f in file_list:
 images[0].save(os.path.join(fs_hm_path, 'first-set-heatmaps.pdf'), save_all=True, append_images=images[1:])
 
 # %%
+gaus_conv_matrix
 
