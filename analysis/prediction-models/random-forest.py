17df87b9d5011f8c725779e3116df46d0ea154de
SethPeacock <msethpeacock@gmail.com>
2022-08-11 09:08:39 -0700

    new firstset plots and random-forest updates

--- analysis/prediction-models/random-forest.py
+++ analysis/prediction-models/random-forest.py
@@ -10,16 +10,16 @@ from evaluation import kfold_log_loss
 data_path = ['data']
 # %%
 X = pd.read_table(os.path.join(*data_path, 'SyntheticData_FirstSet.txt'),
-                       delimiter='   ',
-                       header=None,
-                       dtype=float,
-                       engine='python')
+    delimiter='   ',
+    header=None,
+    dtype=float,
+    engine='python')
 
 y = pd.read_table(os.path.join(*data_path, 'membership.txt'),
-                             delimiter='   ',
-                             names=['motif'],
-                             dtype=int,
-                             engine='python')
+    delimiter='   ',
+    names=['motif'],
+    dtype=int,
+    engine='python')
 
 train_index = y.groupby('motif').sample(frac=.8).index
 test_index = ~y.index.isin(train_index)
@@ -39,19 +39,19 @@ print(GSRF.best_params_)  # {'max_depth': 10, 'max_features': 0.5, 'n_estimators
 rfc_params_1 = {'max_depth': 10, 'max_features': 0.5, 'n_estimators': 100}
 rfc_1 = RandomForestClassifier(**rfc_params_1, n_jobs=-1)
 
-print('Random Forest with Untransformed X, Unreduced y:\n',
-    kfold_log_loss(rfc_1, X=X, y=y)
-) # 3.337138180175846
+logloss_1 = kfold_log_loss(rfc_1, X=X, y=y)  # 3.337138180175846
+print('Random Forest with Untransformed X, Unreduced y\nLogLoss: %s' % logloss_1)
 
-rfc_1.fit(X.loc[train_index], y.loc[train_index].values.reshape(-1))
+rfc_1.fit(X.loc[train_index], y[train_index])
 
-print('Accuracy:',
-    accuracy_score(y.loc[test_index].values.reshape(-1), 
-        rfc_1.predict_proba(X.loc[test_index]))
-)
+acc_1 = accuracy_score(y[test_index], rfc_1.predict(X.loc[test_index]))  # 0.1334002006018054
+print('Accuracy: %s' % acc_1)
 # %% Reduced number of outcomes
 motifs_of_interest = [57, 50, 32, 29, 53, 8, 44]
-y_new = [ i if i in motifs_of_interest else 0 for i in y]
+y_new = [ y_i for y_i in y if y_i in motifs_of_interest]
+X_new_index = [ index for index, y_i in enumerate(y) if y_i in motifs_of_interest]
+
+
 
 '''
 parameters = dict(n_estimators = [100, 500, 1000],
@@ -62,7 +62,14 @@ GSRF = GridSearchCV(RandomForestClassifier(), parameters, n_jobs=-1)
 GSRF.fit(X.loc[train_index], y_new[train_index])
 print(GSRF.best_params_)  # 
 '''
+rfc_2 = RandomForestClassifier(**rfc_params_1, n_jobs=-1)
 
+logloss_2 = kfold_log_loss(rfc_2, X=X, y=y_new)
+print('\nReduced y\nLogLoss: %s' % logloss_2)
+
+rfc_2.fit(X.loc[train_index], y[train_index])
+acc_2 = accuracy_score(y[test_index], rfc_2.predict(X.loc[test_index]))  # 0.1334002006018054
+print('Accuracy: %s' % acc_2)
 
 # %% Transformed X
 X_m = pd.read_csv(os.path.join(*data_path, 'firstset_measures.csv'))
